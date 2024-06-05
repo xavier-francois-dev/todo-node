5959d34ba046e3c4134c170f8d5bac468dd1170d
Xav <xavfranc3@gmail.com>
2024-06-05 19:43:48 +0200

    Persistence configuration/setup CORS/setup body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { DataSource } from "typeorm";
+import * as process from "node:process";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { Task } from "./tasks/tasks.entity";
 
 const app: Express = express();
 dotenv.config();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+export const dataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5435,
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DB,
+  synchronize: true,
+  entities: [Task],
+});
 
 const port = process.env.PORT || 3200;
 
@@ -10,6 +29,14 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + Typescript server");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+dataSource
+  .initialize()
+  .then(() => {
+    console.log("Data source initialized");
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((err) =>
+    console.error("Error during Data Source initialization", err),
+  );
